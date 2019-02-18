21657d50d3be275c927f4a3c18d5004249a154ca
Maycon Heerdt <mayconheerdt@Administrators-MacBook-Pro.local>
2019-02-18 17:42:58 +0000

    last changes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as Controller;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController as Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
